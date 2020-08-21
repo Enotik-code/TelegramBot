1d4dc1060758b94ce22ca8cbc4e49ffd964ef340
Antony <fvantop@gmail.com>
2020-08-21 14:10:59 +0300

    21.08

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
       ApiContextInitializer.init();
@@ -13,5 +16,6 @@ public class Main {
         } catch (TelegramApiRequestException e) {
             e.printStackTrace();
         }
+        SpringApplication.run(Main.class, args);
     }
 }
